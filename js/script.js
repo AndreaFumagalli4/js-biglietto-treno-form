e1d56d3d59a87b4d5f42af1987ffecbae9f5ecab
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-22 15:53:02 +0100

    add addEventListener calc

--- js/script.js
+++ js/script.js
@@ -6,11 +6,35 @@
 3. Mostrare all'utente il prezzo del biglietto.
 */
 
-const distance = document.getElementById('distance');
+// 1. Distance and age
 
-console.log(distance);
+const distance = document.getElementById('distance');
 
 const age = document.getElementById('age');
 
-console.log(age);
+const output = document.getElementById('output');
+
+// 2. Ticket count
+
+const button = document.querySelector('a button');
+
+button.addEventListener('click', function(){
+
+    let ticketPrice = distance.value * 0.2762;
+
+    console.log(distance.value);
+    console.log(age.value);
+    console.log(ticketPrice);
+    
+    if ( age.value < 18 ){
+        ticketPrice = ticketPrice * 0.825;
+    } else if ( age.value > 65 ){
+        ticketPrice = ticketPrice * 0.667;
+    }
+
+    console.log(ticketPrice);
+
+    output.innerHTML = 'Il prezzo del biglietto è di ' + ticketPrice.toFixed(2) + '&euro;'; 
+});
+
 
