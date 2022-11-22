67a4e400d2ccc70180f3daba4135ee73f674a013
AndreaFumagalli4 <98.fumagalli.andrea@gmail.com>
2022-11-22 15:11:43 +0100

    take people data

---
+++ js/script.js
@@ -0,0 +1,16 @@
+/**
+1. Chiedere i chilometri da percorrere e l'età del passeggero/passeggera
+2. Calcolare il costo del biglietto (SOLO AL CLICK AVANTI!!!):
+    2.1 Se il passeggero/a è minorenne => 17.5% di sconto;
+    2.2 Se il passeggero/a è over 65=> 33.3% di sconto.
+3. Mostrare all'utente il prezzo del biglietto.
+*/
+
+const distance = document.getElementById('distance');
+
+console.log(distance);
+
+const age = document.getElementById('age');
+
+console.log(age);
+
